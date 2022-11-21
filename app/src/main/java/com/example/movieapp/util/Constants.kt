42727f210a1654d1f3c13c684284b9c1a24d470c
Atharv jain <84101268+Atharva-jain@users.noreply.github.com>
2022-11-21 12:53:03 +0530

    Update Constants.kt

--- app/src/main/java/com/example/movieapp/util/Constants.kt
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -2,9 +2,9 @@ package com.example.movieapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "f92857244f927ea9f1bd8dd74cbc1040"
-        const val YOUTUBE_API_KEY = "AIzaSyD1Mz1pi8ICjR5HoMz4d_sYp5bkZ9ut0Uk"
-        const val BASE_URL = "https://api.themoviedb.org/"
+        const val API_KEY = ""
+        const val YOUTUBE_API_KEY = ""
+        const val BASE_URL = ""
         const val SEARCH_MOVIE_TIME_DELAY = 500L
         const val EN_US = "en-US"
     }
